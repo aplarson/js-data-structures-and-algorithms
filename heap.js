d8242a5c5382fab13fde966eb8cb847b1caf6ea7
aplarson <andrew.p.larson@gmail.com>
2015-04-13 13:20:55 -0700

    Hoist heap variables

--- heap.js
+++ heap.js
@@ -4,8 +4,9 @@ function BTreeHeap () {
 }
 
 BTreeHeap.prototype.childVals = function (parentIdx) {
-  var left = this.heap[2 * parentIdx + 1];
-  var right = this.heap[(2 * parentIdx) + 2];
+  var left, right;
+  left = this.heap[2 * parentIdx + 1];
+  right = this.heap[(2 * parentIdx) + 2];
   if (typeof left === "undefined") {
     return [];
   } else if (typeof right === "undefined") {
@@ -27,16 +28,17 @@ BTreeHeap.prototype.extract = function () {
 };
 
 BTreeHeap.prototype.heapifyDown = function (parentIdx) {
+  var childVals, values, min, minIdx, childIdx, switchVal;
   if (!this.heap[parentIdx]) {
     return false;
   }
-  var childVals = this.childVals(parentIdx);
-  var values = [this.heap[parentIdx]].concat(childVals);
-  var min = Math.min.apply(Math, values);
-  var minIdx = values.indexOf(min);
+  childVals = this.childVals(parentIdx);
+  values = [this.heap[parentIdx]].concat(childVals);
+  min = Math.min.apply(Math, values);
+  minIdx = values.indexOf(min);
   if (minIdx !== 0) {
-    var childIdx = (2 * parentIdx) + minIdx;
-    var switchVal = this.heap[childIdx];
+    childIdx = (2 * parentIdx) + minIdx;
+    switchVal = this.heap[childIdx];
     this.heap[childIdx] = this.heap[parentIdx];
     this.heap[parentIdx] = switchVal;
     this.heapifyDown(childIdx);
@@ -45,13 +47,14 @@ BTreeHeap.prototype.heapifyDown = function (parentIdx) {
 };
 
 BTreeHeap.prototype.insert = function (value) {
+  var curIdx, parentIdx;
   this.heap.push(value);
-  var curIdx = this.heap.length - 1;
-  var parentIdx = Math.floor((curIdx - 1) / 2);
+  curIdx = this.heap.length - 1;
+  parentIdx = Math.floor((curIdx - 1) / 2);
   while (value < this.heap[parentIdx]) {
     this.heap[curIdx] = this.heap[parentIdx];
     this.heap[parentIdx] = value;
-    curIdx = parentIdx
+    curIdx = parentIdx;
     parentIdx = Math.floor((curIdx - 1) / 2);
   }
   this.length += 1;
